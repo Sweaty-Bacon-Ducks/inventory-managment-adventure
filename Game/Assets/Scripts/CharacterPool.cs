e197e89d1d3cbaa0387964b5b9e7b2ab47e446ce
Onjefi <ejnofilip@gmail.com>
2018-08-12 00:50:11 +0200

    RNG i AJTEMY
    3===D

---
+++ Game/Assets/Scripts/CharacterPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterPool : ScriptableObject
+{
+    [SerializeField] private List<Character> pool;
+
+    public List<Character>GetRandom(int number)
+    {
+        List<Character> generatedList = new List<Character>();
+
+        for (int i = 0; i < number; i++)
+        {
+            generatedList.Add(pool[Random.Range(0, pool.Count)]);
+        }
+
+        return generatedList;
+    }
+	
+}
---
+++ Game/Assets/Scripts/HealingPotion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Item",menuName ="Inventory/Healing Potion")]
+public class HealingPotion : UsableItem
+{
+    [SerializeField] private float healingValue;
+
+    public override void Use()
+    {
+
+    }
+	
+}
