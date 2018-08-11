8494de871e21bbe673b386abb8b917a820959c47
Onjefi <32872382+Onjefi@users.noreply.github.com>
2018-08-11 16:20:25 +0200

    Feature interface item (#8) (#9)
    * Item interface, inventory, weapon and armor scripts
    
    * Messaging system (#7)
    
    * Poprawki

---
+++ Game/Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	private List<InventoryItem> items;
+
+	public void AddToInventory(InventoryItem item)
+	{
+		if (item != null)
+		{
+			items.Add(item);
+		}
+	}
+
+	public bool RemoveFromInventory(InventoryItem item)
+	{
+		return items.Remove(item);
+	}
+
+	public bool RemoveAllFromInventory()
+	{
+		return items.RemoveAll(x => x) > 0;
+	}
+}
