c43c399414210c4eb549e5703ac5ec5dcac1c914
Unknown <kowalik.kub@gmail.com>
2018-08-12 00:45:06 +0200

    Changed usable item interface

--- Game/Assets/Scripts/Weapon.cs
+++ Game/Assets/Scripts/Weapon.cs
@@ -7,10 +7,34 @@ public class Weapon : InventoryItem
 {
 	[SerializeField]
 	private float maxDamage = 0.1f;
+
+	public float MaxDamage
+	{
+		get
+		{
+			return maxDamage;
+		}
+	}
+
 	[SerializeField]
 	private float minDamage = 0.1f;
 
+	public float MinDamage
+	{
+		get
+		{
+			return minDamage;
+		}
+	}
+
 	[SerializeField]
 	private float attackSpeed = 0.1f;
-	
+
+	public float AttackSpeed
+	{
+		get
+		{
+			return attackSpeed;
+		}
+	}
 }
