d64444b5c805bf1fc98932c1d9c35541c6ad9c19
Kacper Obara <kacper97pl@tlen.pl>
2018-08-11 17:22:06 +0200

    Ui (#11)
    * Initial
    
    * poprawki

---
+++ Game/Assets/Scripts/UI/ItemDragDrop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemDragged;
+    public static Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemDragged = gameObject;
+        startParent = transform.parent;
+
+        transform.SetParent(transform.root);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        AudioManager.instance.Play("ItemDrop");
+
+        if (transform.parent == startParent || transform.parent == transform.root)
+        {
+            Debug.Log("Miejsce sie nie zmienilo");
+            transform.SetParent(startParent);
+        }
+        
+        transform.localPosition = Vector3.zero;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        itemDragged = null;
+        startParent = null;
+    }
+}
---
+++ Game/Assets/Scripts/UI/Panel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Panel : MonoBehaviour, IDropHandler {
+
+    // If player dropped the item on this panel
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (ItemDragDrop.itemDragged != null)
+            ItemDragDrop.itemDragged.transform.SetParent(transform.GetChild(0));
+    }
+}
