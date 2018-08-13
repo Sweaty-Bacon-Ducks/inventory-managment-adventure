01f102319112cbb83bd48213bfcdd8a3d8108aa8
Onjefi <32872382+Onjefi@users.noreply.github.com>
2018-08-13 22:11:30 +0200

    Event manager (#35)
    * Manager
    
    * drobne
    
    * event list
    
    * update
    
    * awaiter
    
    * AWAAAITER

---
+++ Game/Assets/Scripts/RandomEventManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEventManager : MonoBehaviour
+{
+    [SerializeField] private float eventChance;
+    [SerializeField] private float frequency;
+    [SerializeField] private List<EventSO> eventList;
+
+    public float Frequency { set { frequency = Mathf.Max(value, 0.01f); } get { return frequency; } }
+    public float EventChance { set { eventChance = Mathf.Lerp(0f, 1f, value); } get { return eventChance; } }
+    void OnValidate()
+    {
+        frequency = Mathf.Max(frequency, 0.01f);
+        eventChance = Mathf.Lerp(0f, 1f, eventChance);
+    }
+
+    private bool stateSwitch = false;
+	
+    private IEnumerator Overwatch()
+    { 
+
+        while (stateSwitch)
+        {
+            EventCheck();
+            yield return new WaitForSeconds(frequency);
+        }
+    }
+
+    private void EventCheck()
+    {
+        if (Random.Range(0, 100) <= (eventChance * 100)) Execute();
+
+    }
+
+    private void Execute()
+    {
+        REMOff();
+        eventList[Random.Range(0, eventList.Count)].ExecuteEvent();
+        REMOn();
+    }
+
+    public void REMOn()
+    {
+        if (!stateSwitch)
+        {
+            stateSwitch = true;
+            StartCoroutine(Overwatch());
+        }
+        
+    }
+
+    public void REMOff()
+    {
+        stateSwitch=false;
+    }
+
+
+
+
+}
