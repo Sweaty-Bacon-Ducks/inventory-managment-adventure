ce41c2add70afe9bdd744be08e7a42e59e3831ee
Onjefi <ejnofilip@gmail.com>
2018-08-13 16:26:19 +0200

    awaiter

---
+++ Game/Assets/InputAwaiter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public delegate void CallBack(bool response);
+
+public class InputAwaiter : MonoBehaviour
+{
+    private bool ocupied;
+    private List<CallBack> query = new List<CallBack>();
+    private static InputAwaiter instance;
+    public static InputAwaiter Instance { get { return instance; } }
+
+
+    void Awake()
+    {
+        if (instance!=null&&instance!=this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    
+    public void GetRespose(CallBack callBack)
+    {
+        
+        if (!ocupied)
+        {
+            ocupied = true;
+            StartCoroutine(WaitForResponse(callBack));
+        }
+        else
+        {
+            query.Add(callBack);
+        }
+        
+    }
+
+    private IEnumerator WaitForResponse(CallBack callBack)
+    {
+        //while (!y || !n)
+        //{
+        //    if (Input.GetKey("y")) response = true;
+        //    if (Input.GetKey("n")) response = false;
+        //    yield return null;
+        //}
+        for (int i = 0; i < 5; i++)
+        {
+            Logger.Instance.ShowLog(DateTime.Now, (i+1).ToString());
+            yield return new WaitForSeconds(1);
+        }
+        callBack(true);
+        ocupied = false;
+        GetRespose(query[1]);
+        query.RemoveAt(1);
+
+        
+
+    }
+}
--- Game/Assets/Scripts/ChestEvent.cs
+++ Game/Assets/Scripts/ChestEvent.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestEvent : MonoBehaviour {
+[CreateAssetMenu(fileName ="New Object",menuName ="Events/ChestEvent")]
+public class ChestEvent : EventSO
+{
+    CallBack callback;
+    [SerializeField] private string x;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public override void ExecuteEvent()
+    {
+        callback = new CallBack(Continue);
+        InputAwaiter.Instance.GetRespose(callback);
+    }
+
+
+    
+   
+    public void Continue(bool response)
+    {
+        Logger.Instance.ShowLog(System.DateTime.Now, x);
+    }
+
+    
+
 }
